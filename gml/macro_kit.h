1ba767ea26ba90915bc11d7a1190fdc8143ede0d
YohnWang <347220312@qq.com>
2022-03-26 00:01:05 +0800

    fix built-in array static assert

--- gml/macro_kit.h
+++ gml/macro_kit.h
@@ -103,7 +103,7 @@
 #define type_is_numeric(T) (_Generic((typeof(T)){0},signed char:1,unsigned char:1,short:1,unsigned short:1,int:1,unsigned int:1,long:1,unsigned long:1,long long:1,unsigned long long:1,float:1,double:1,long double:1,default:0))
 
 // static assert
-#define static_assert_is_builtin_array(a) _Static_assert(is_builtin_array(a),"not an array")
+#define static_assert_is_builtin_array(a) _Static_assert((a)==&(a),"not an array")
 #define static_assert_type_is_same_0(T1,T2) _Static_assert(type_is_same(T1,T2),"type is not same")
 #define static_assert_type_is_same_1(T1,T2,errstr) _Static_assert(type_is_same(T1,T2),errstr)
 #define static_assert_type_is_same(T1,T2,...) macro_cat(static_assert_type_is_same_,count_macro_args(__VA_ARGS__))(T1,T2,##__VA_ARGS__)
