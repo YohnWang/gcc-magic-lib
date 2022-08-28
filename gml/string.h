ec671cbc9c0aebf4a2c5b3225851e0b3de24c7d6
YohnWang <347220312@qq.com>
2022-08-28 23:33:52 +0800

    modify string

--- gml/string.h
+++ gml/string.h
@@ -1,113 +1,70 @@
+// This file is from https://github.com/YohnWang/gcc-magic-lib
 #ifndef GML_STRING_H
 #define GML_STRING_H
 
 #include"vector.h"
 #include<stdio.h>
 #include<stdbool.h>
+#include<ctype.h>
 
-typedef char gml_vstring_char_t;
-vector_def(gml_vstring_char_t)
-typedef vector(gml_vstring_char_t) vstring;
-vector_def(vstring)
+typedef char string_char_t;
+vector_def(string_char_t);
 
-#define vstring_split_raii attr_cleanup(vstring_split_del)
-#define vstring_split_t vector(vstring)
-
-static inline void vstring_split_del(vector(vstring) *split)
+typedef struct string
 {
-    for(ssize_t i=0;i<vector_size(split);i++)
-        vector_del(vector_at(split,i));
-    vector_del(split);
-}
+    vector(string_char_t) v;
+}string;
 
-#define vstring_getline(...) macro_cat(vstring_getline_,count_macro_args(__VA_ARGS__))(__VA_ARGS__)
+#define string_raii attr_cleanup(string_del)
 
-static inline bool vstring_getline_2(vstring *vs,FILE *fp)
+void string_init(string *s);
+void string_del(string *s);
+void string_cat_string(string *s,const string *d);
+void string_cat_cstring(string *s,const char *d);
+string cstring_to_string(const char *d);
+string uint_to_string(uint64_t x);
+string int_to_string(int64_t x);
+int string_cmp_string(const string *s,const string *d);
+int string_cmp_cstring(const string *s,const char *d);
+bool string_eq_string(const string *s,const string *d);
+bool string_eq_cstring(const string *s,const char *d);
+const char* string_cstr(const string *s);
+
+
+#define string_size(s) (vector_size(&s->v))
+#define string_ref(s,i)  (vector_ref(&s->v,i))
+
+#define to_string(x) _Generic(x,char*:cstring_to_string,const char*:cstring_to_string,\
+                                signed char:int_to_string,short:int_to_string,int:int_to_string,long:int_to_string,long long:int_to_string,\
+                                unsigned char:uint_to_string,unsigned short:uint_to_string,unsigned int:uint_to_string,unsigned long:uint_to_string,unsigned long long:uint_to_string)(x)
+
+#define string_cat(s,d) _Generic(d,char*:string_cat_cstring,const char*:string_cat_cstring,\
+                                   string*:string_cat_string,const string*:string_cat_string)(s,d)
+
+
+#define string_cmp(s,d) _Generic(d,char*:string_cmp_cstring,const char*:string_cmp_cstring,\
+                                   string*:string_cmp_string,const string*:string_cmp_string)(s,d)
+
+#define string_eq(s,d) _Generic(d,char*:string_eq_cstring,const char*:string_eq_cstring,\
+                                   string*:string_eq_string,const string*:string_eq_string)(s,d)
+
+#define string_ne(s,d) (!string_eq(s,d))
+#define string_lt(s,d) (string_cmp(s,d)<0)
+#define string_le(s,d) (string_cmp(s,d)<=0)
+#define string_gt(s,d) (string_cmp(s,d)>0)
+#define string_ge(s,d) (string_cmp(s,d)>=0)
+
+typedef string sub_string_t;
+vector_def(sub_string_t);
+typedef vector(sub_string_t) string_splite_t;
+#define string_splite_raii attr_cleanup(string_splite_del)
+
+static void string_splite_del(string_splite_t *v)
 {
-    int c;
-    vector_clear(vs);
-    while((c=fgetc(fp))!=EOF && c!='\n')
-        vector_push_back(vs,c);
-    bool read_state=!vector_empty(vs);
-    vector_push_back(vs,'\0');
-    return read_state;
-}
-
-static inline bool vstring_getline_1(vstring *vs)
-{
-    return vstring_getline(vs,stdin);
-}
-
-static inline char* vstring_data(vstring *vs)
-{
-    if(!vector_empty(vs))
-        return vector_data(vs);
-    return "";
-}
-
-#define vstring_init(vs1,vs2) _Generic(vs2,char*:vstring_init_builtin_string,const char*:vstring_init_builtin_string,default:vstring_init_vstring)(vs1,vs2)
-
-static inline void vstring_init_builtin_string(vstring *vs,const char *s)
-{
-    vector_clear(vs);
-    for(size_t i=0;s[i]!='\0';i++)
-        vector_push_back(vs,s[i]);
-    vector_push_back(vs,'\0');
-}
-
-static inline void vstring_init_vstring(vstring *vs,vstring *vs2)
-{
-    vector_clear(vs);
-    vector_assign(vs,vs2);
-}
-
-static inline void vstring_split(vstring *vs,char *sp,vector(vstring) *v)
-{
-    vector_del(v);
-    bool char_map[256]={};
-    for(size_t i=0;sp[i]!='\0';i++)
+    for(ssize_t i=0;i<vector_size(v);i++)
     {
-        char_map[(size_t)sp[i]]=true;
-    }
-    vstring tmp={};
-    for(ssize_t i=0;i<vector_size(vs);i++)
-    {
-        if(char_map[(size_t)vector_ref(vs,i)]==false)
-        {
-            vector_push_back(&tmp,vector_ref(vs,i));
-        }
-        else
-        {
-            vector_push_back(&tmp,'\0');
-            vector_push_back(v,tmp);
-            vector_init(&tmp);
-        }
-    }
-    if(!vector_empty(&tmp))
-    {
-        if(vector_back(&tmp)!='\0')
-            vector_push_back(&tmp,'\0');
-        vector_push_back(v,tmp);
+        string_del(vector_at(v,i));
     }
 }
-
-#define vstring_cat(vs1,vs2) _Generic(vs2,char*:vstring_cat_builtin_string,const char*:vstring_cat_builtin_string,default:vstring_cat_vstring)(vs1,vs2)
-
-static void vstring_cat_vstring(vstring *vs1,vstring *vs2)
-{
-    if(!vector_empty(vs1)&&!vector_empty(vs2))
-        vector_pop_back(vs1);
-    for(ssize_t i=0;i<vector_size(vs2);i++)//vector_ref(vs2,i)!='\0'
-        vector_push_back(vs1,vector_ref(vs2,i));
-}
-
-static void vstring_cat_builtin_string(vstring *vs1,const char *s)
-{
-    if(!vector_empty(vs1))
-        vector_pop_back(vs1);
-    for(ssize_t i=0;s[i]!='\0';i++)
-        vector_push_back(vs1,s[i]);
-    vector_push_back(vs1,'\0');
-}
-
+string_splite_t string_splite(const string *s,string_char_t c);
 #endif
--- gmllib.c
+++ gmllib.c
@@ -7,10 +7,169 @@ void throw_base(int e)
     if(_g_jmp_buf_ptr==NULL)
     {
         fprintf(stderr,"terminate called after throwing error code %d\n",e);
-        abort();
+        terminate();
     }
     if(e)
         longjmp(*_g_jmp_buf_ptr,e);
+}
+
+void string_init(string *s)
+{
+    vector_init(&s->v);
+}
+
+void string_del(string *s)
+{
+    vector_del(&s->v);
+}
+
+void string_cat_string(string *s,const string *d)
+{
+    for(ssize_t i=0;i<vector_size(&d->v);i++)
+    {
+        vector_push_back(&s->v,vector_ref(&d->v,i));
+    }
+}
+
+void string_cat_cstring(string *s,const char *d)
+{
+    for(ssize_t i=0;d[i]!='\0';i++)
+    {
+        vector_push_back(&s->v,d[i]);
+    }
+}
+
+string cstring_to_string(const char *d)
+{
+    string s={};
+    string_cat_cstring(&s,d);
+    return s;
+}
+
+#pragma GCC diagnostic ignored "-Wclobbered"
+
+string uint_to_string(uint64_t x)
+{
+    if(x==0)
+        return cstring_to_string("0");
+    string s={};
+    try
+    {
+        while(x!=0)
+        {
+            vector_push_back(&s.v,x%10+'0');
+            x/=10;
+        }
+        array_reverse(vector_data(&s.v),vector_size(&s.v));
+    }
+    catch(int e)
+    {
+        string_del(&s);
+        throw(e);
+    }
+
+    return s;
+}
+
+string int_to_string(int64_t x)
+{
+    if(x==0)
+        return cstring_to_string("0");
+    if(x>0)
+    {
+        return uint_to_string(x);
+    }
     else
-        longjmp(*_g_jmp_buf_ptr,(error_unknow));
+    {
+        x=-x;
+        if(x<0)
+            return cstring_to_string("-9223372036854775808");
+        else
+            return uint_to_string(x);
+    }
+}
+
+int string_cmp_string(const string *s,const string *d)
+{
+    if(vector_size(&s->v)==0&&vector_size(&d->v)!=0)
+        return -1;
+    else if(vector_size(&s->v)!=0&&vector_size(&d->v)==0)
+        return 1;
+    
+    for(ssize_t i=0;i<vector_size(&s->v)&&i<vector_size(&d->v);i++)
+    {
+        if(vector_ref(&s->v,i)<vector_ref(&d->v,i))
+            return -1;
+        else if(vector_ref(&s->v,i)>vector_ref(&d->v,i))
+            return 1;
+    }
+    return 0;
+}
+
+int string_cmp_cstring(const string *s,const char *d)
+{
+    if(vector_size(&s->v)==0&&d[0]!='\0')
+        return -1;
+    else if(vector_size(&s->v)!=0&&d[0]=='\0')
+        return 1;
+
+    for(ssize_t i=0;i<vector_size(&s->v)&&d[i]!='\0';i++)
+    {
+        if(vector_ref(&s->v,i)<d[i])
+            return -1;
+        else if(vector_ref(&s->v,i)>d[i])
+            return 1;
+    }
+    return 0;
+}
+
+bool string_eq_string(const string *s,const string *d)
+{
+    if(vector_size(&s->v)!=vector_size(&d->v))
+        return false;
+    return string_cmp_string(s,d)==0;
+}
+
+bool string_eq_cstring(const string *s,const char *d)
+{
+    return string_cmp_cstring(s,d)==0;
+}
+
+const char* string_cstr(const string *_s)
+{
+    string *s=(string*)_s;
+    vector_push_back(&s->v,'\0');
+    vector_pop_back(&s->v);
+    return vector_data(&s->v);
+}
+
+string_splite_t string_splite(const string *s,string_char_t c)
+{
+    string_splite_t v={};
+    string t={};
+    try
+    {
+        for(ssize_t i=0;i<string_size(s);i++)
+        {
+            if(string_ref(s,i)!=c)
+                vector_push_back(&t.v,string_ref(s,i));
+            else
+            {
+                vector_push_back(&v,t);
+                string_init(&t);
+            }
+        }
+        if(string_ne(&t,""))
+        {
+            vector_push_back(&v,t);
+        }
+    }
+    catch(int e)
+    {
+        string_del(&t);
+        string_splite_del(&v);
+        throw(e);
+    }
+
+    return v;
 }
