fc164be66f392470aa95efa7bc1118a5cfe64a4d
YohnWang <347220312@qq.com>
2022-10-05 00:18:28 +0800

    modify static assert of built-in array

--- gml/type_traits.h
+++ gml/type_traits.h
@@ -10,15 +10,15 @@
 #define type_is_same(T1,T2) (_Generic((typeof(T1)*){0},typeof(T2)*:1,default:0))
 #define type_is_integral(T) (_Generic((typeof(T)){0},signed char:1,unsigned char:1,short:1,unsigned short:1,int:1,unsigned int:1,long:1,unsigned long:1,long long:1,unsigned long long:1,default:0))
 #define type_is_float(T) (_Generic((typeof(T)){0},float:1,double:1,long double:1,default:0))
-#define type_is_equal_helper(T1,T2) (_Generic((typeof(T1)){0},typeof(T2):1,default:0))
-#define type_is_equal(T1,T2) (type_is_equal_helper(T1,T2)||type_is_equal_helper(T2,T1))
+#define type_is_bool(T) _Generic((typeof(T)){0},bool:1,default:0)
+#define type_is_equal(T1,T2) __builtin_types_compatible_p(typeof(T1),typeof(T2))
 #define type_is_signed(T) (_Generic((typeof(T)){0},signed char:1,short:1,int:1,long:1,long long:1,float:1,double:1,default:0))
 #define type_is_unsigned(T) (_Generic((typeof(T)){0},unsigned char:1,unsigned short:1,unsigned int:1,unsigned long:1,unsigned long long:1,default:0))
 #define type_is_same_sign(T1,T2) (type_is_signed(T1)==type_is_signed(T2))
 #define type_is_numeric(T) (_Generic((typeof(T)){0},signed char:1,unsigned char:1,short:1,unsigned short:1,int:1,unsigned int:1,long:1,unsigned long:1,long long:1,unsigned long long:1,float:1,double:1,long double:1,default:0))
 
 // static assert
-#define static_assert_is_builtin_array(a) ({__attribute__((unused)) typeof(a) x;_Static_assert((intptr_t)(&x[0])==(intptr_t)&(x),"not an array");})
+#define static_assert_is_builtin_array(a) _Static_assert(type_is_equal(a,typeof(a[0])[]),"not an array")
 #define static_assert_type_is_same_0(T1,T2) _Static_assert(type_is_same(T1,T2),"type is not same")
 #define static_assert_type_is_same_1(T1,T2,errstr) _Static_assert(type_is_same(T1,T2),errstr)
 #define static_assert_type_is_same(T1,T2,...) macro_cat(static_assert_type_is_same_,count_macro_args(__VA_ARGS__))(T1,T2,##__VA_ARGS__)
