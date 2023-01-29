fc01dc784a4fd552b4493a60bbeeb6b7874e1140
YohnWang <347220312@qq.com>
2023-01-30 00:43:22 +0800

    update map, add map_erase

--- gml/avl.h
+++ gml/avl.h
@@ -59,12 +59,34 @@
     avl_rr_rotate(k3);\
 })
 
-#define avl_tree_default_compare(x,y) ((x)<(y))
+#define avl_most_left(root) \
+({\
+    typedef typeof(root) tree_node_t;\
+    tree_node_t avl_left_most(tree_node_t r)\
+    {\
+        auto p=r;\
+        while(p!=NULL) p=p->left;\
+        return p;\
+    }\
+    avl_left_most(root);\
+})
+
+#define avl_most_right(root) \
+({\
+    typedef typeof(root) tree_node_t;\
+    tree_node_t avl_right_most(tree_node_t r)\
+    {\
+        auto p=r;\
+        while(p!=NULL) p=p->right;\
+        return p;\
+    }\
+    avl_right_most(root);\
+})
 
 #define avl_tree_find(root,_key,compare) \
 ({\
     typedef typeof(root) tree_node_t;\
-    typedef typeof(_key) key_t;\
+    typedef typeof((root)->key) key_t;\
     tree_node_t avl_find(tree_node_t r,key_t k)\
     {\
         tree_node_t p=r;\
@@ -89,16 +111,13 @@
     {\
         if(r==NULL)\
         {\
-            n->left=NULL;\
-            n->right=NULL;\
-            n->height=0;\
             return n;\
         }\
         if(compare(n->key,r->key))\
         {\
             r->left=avl_add(r->left,n);\
-            int hl=avl_get_height(r->left);\
-            int hr=avl_get_height(r->right);\
+            auto hl=avl_get_height(r->left);\
+            auto hr=avl_get_height(r->right);\
             if(hl-hr==2)\
             {\
                 if(compare(n->key,r->left->key))\
@@ -110,8 +129,8 @@
         else if(compare(r->key,n->key))\
         {\
             r->right=avl_add(r->right,n);\
-            int hl=avl_get_height(r->left);\
-            int hr=avl_get_height(r->right);\
+            auto hl=avl_get_height(r->left);\
+            auto hr=avl_get_height(r->right);\
             if(hr-hl==2)\
             {\
                 if(compare(n->key,r->right->key))\
@@ -124,6 +143,73 @@
         return r;\
     }\
     avl_add(root,node);\
+})
+
+#define avl_tree_del(root,node,compare) \
+({\
+    typedef typeof(root) tree_node_t;\
+    typedef typeof((root)->key) key_t;\
+    tree_node_t move_out_node=NULL;\
+    tree_node_t avl_del(tree_node_t r,tree_node_t n)\
+    {\
+        if(r==NULL) return NULL;\
+        if(compare(n->key,r->key))\
+        {\
+            r->left=avl_del(r->left,n);\
+            auto hl=avl_get_height(r->left);\
+            auto hr=avl_get_height(r->right);\
+            if(hr-hl==2)\
+            {\
+                auto right=r->right;\
+                if(avl_get_height(right->left)>avl_get_height(right->right))\
+                    r=avl_rl_rotate(r);\
+                else\
+                    r=avl_rr_rotate(r);\
+            }\
+        }\
+        else if(compare(r->key,n->key))\
+        {\
+            r->right=avl_del(r->right,n);\
+            auto hl=avl_get_height(r->left);\
+            auto hr=avl_get_height(r->right);\
+            if(hl-hr==2)\
+            {\
+                auto left=r->left;\
+                if(avl_get_height(left->right)>avl_get_height(left->left))\
+                    r=avl_lr_rotate(r);\
+                else\
+                    r=avl_ll_rotate(r);\
+            }\
+        }\
+        else\
+        {\
+            if(r->left&&r->right)\
+            {\
+                if(avl_get_height(r->left)>avl_get_height(r->right))\
+                {\
+                    auto p=avl_most_right(r->left);\
+                    swap(&r->key,&p->key);\
+                    r->left=avl_del(r->left,p);\
+                }\
+                else\
+                {\
+                    auto p=avl_most_left(r->right);\
+                    swap(&r->key,&p->key);\
+                    r->right=avl_del(r->right,p);\
+                }\
+            }\
+            else\
+            {\
+                auto p=r;\
+                r=r->left?r->left:r->right;\
+                move_out_node=p;\
+            }\
+        }\
+        return r;\
+    }\
+    auto r=avl_del(root,node);\
+    tuple_def(tree_node_t,tree_node_t);\
+    (tuple(tree_node_t,tree_node_t)){r,move_out_node};\
 })
 
 #endif
--- gml/map.h
+++ gml/map.h
@@ -6,21 +6,26 @@
 
 #define map(key_type,value_type) macro_cat(_g_map_,key_type,_,value_type)
 #define _g_map_node(key_type,value_type) macro_cat(_g_map_node,key_type,_,value_type)
+#define map_tuple(key_type,value_type) tuple(macro_cat(_g_map,key_type),macro_cat(_g_map,value_type))
 
 #define map_def(key_type,value_type) \
+typedef typeof(key_type) macro_cat(_g_map,key_type);\
+typedef typeof(value_type) macro_cat(_g_map,value_type);\
+tuple_def(macro_cat(_g_map,key_type),macro_cat(_g_map,value_type));\
 typedef struct _g_map_node(key_type,value_type) \
 {\
     struct _g_map_node(key_type,value_type) *left;\
     struct _g_map_node(key_type,value_type) *right;\
     ssize_t height;\
-    key_type key;\
-    value_type value;\
+    map_tuple(key_type,value_type) key;\
 }_g_map_node(key_type,value_type);\
 typedef struct map(key_type,value_type) \
 {\
     _g_map_node(key_type,value_type) *root;\
     ssize_t size;\
 }map(key_type,value_type)
+
+#define _g_map_default_compare(x,y) (tuple_get(x,0)<tuple_get(y,0))
 
 #define map_init(map) \
 ({\
@@ -29,24 +34,63 @@ typedef struct map(key_type,value_type) \
     m->size=0;\
 })
 
-#define map_find(map,key)\
+#define map_find(...) macro_cat(_g_map_find_,count_macro_args(__VA_ARGS__))(__VA_ARGS__)
+#define _g_map_find_2(map,_key) _g_map_find_3(map,_key,_g_map_default_compare)
+#define _g_map_find_3(map,_key,compare)\
 ({\
     auto m=map;\
-    avl_tree_find(m->root,key,avl_tree_default_compare);\
+    typedef typeof(m->root->key) key_t;\
+    avl_tree_find(m->root,(key_t){_key},compare);\
 })
 
-#define map_contains(map,key)\
+#define map_contains(...) macro_cat(_g_map_contains_,count_macro_args(__VA_ARGS__))(__VA_ARGS__)
+#define _g_map_contains_2(map,_key) _g_map_contains_3(map,_key,_g_map_default_compare)
+#define _g_map_contains_3(map,key,compare)\
 ({\
-    (bool)(map_find(map,key)!=NULL);\
+    (bool)(map_find(map,key,compare)!=NULL);\
 })
 
-#define map_insert(map,_key,_value) \
+#define map_insert(...) macro_cat(_g_map_insert_,count_macro_args(__VA_ARGS__))(__VA_ARGS__)
+#define _g_map_insert_3(map,_key,_value) _g_map_insert_4(map,_key,_value,_g_map_default_compare)
+#define _g_map_insert_4(map,_key,_value,compare) \
 ({\
     auto m=map;\
     typedef typeof(*(m->root)) node_t;\
-    auto n=new(node_t,.key=_key,.value=_value);\
-    m->root=avl_tree_add(m->root,n,avl_tree_default_compare);\
+    auto n=new(node_t,.key={_key,_value});\
+    m->root=avl_tree_add(m->root,n,compare);\
     m->size++;\
+})
+
+#define map_ref(...) (*_g_map_ref(__VA_ARGS__)) 
+#define _g_map_ref(...) macro_cat(_g_map_ref_,count_macro_args(__VA_ARGS__))(__VA_ARGS__)
+#define _g_map_ref_2(map,_key) _g_map_ref_3(map,_key,_g_map_default_compare)
+#define _g_map_ref_3(map,_key,compare) \
+({\
+    typedef typeof(map) map_t;\
+    typedef typeof(tuple_get((map)->root->key,0)) key_t;\
+    typedef typeof(tuple_get((map)->root->key,1)) value_t;\
+    value_t* map_ref_helper(map_t m,key_t k)\
+    {\
+        auto target=map_find(m,k,compare);\
+        if(target!=NULL) return &tuple_get(target->key,1);\
+        map_insert(m,k,(value_t){0},compare);\
+        target=map_find(m,k,compare);\
+        return &tuple_get(target->key,1);\
+    }\
+    map_ref_helper(map,_key);\
+})
+
+#define map_erase(...) macro_cat(_g_map_erase_,count_macro_args(__VA_ARGS__))(__VA_ARGS__)
+#define _g_map_erase_2(map,_key) _g_map_erase_3(map,_key,_g_map_default_compare)
+#define _g_map_erase_3(map,key,compare) \
+({\
+    auto m=map;\
+    auto k=key;\
+    auto n=map_find(m,k);\
+    auto ret=avl_tree_del(m->root,n,compare);\
+    m->root=tuple_get(ret,0);\
+    delete(tuple_get(ret,1));\
+    m->size--;\
 })
 
 #define map_size(map) ((map)->size)
