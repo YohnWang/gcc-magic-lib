7dee3f6b0c1187ff834a8c9d2bc50cb0073a53ef
YohnWang <347220312@qq.com>
2023-02-27 01:10:41 +0800

    add println

--- gml/print.h
+++ gml/print.h
@@ -77,6 +77,8 @@ static inline void _g_print_cstring(const char *s)
 #include"_print.inc"
 #define print(...) macro_cat(_g_print_,count_macro_args(__VA_ARGS__))(__VA_ARGS__)
 
+#define println(...) print(__VA_ARGS__,"\n")
+
 #include"_place_point.inc"
 #define _g_place_point_0() print(where_from(),"\n")
 #define place_point(...) macro_cat(_g_place_point_,count_macro_args(__VA_ARGS__))(__VA_ARGS__)
