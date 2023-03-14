5b339690e2a47e136b0270dcf721558b7a7debe5
YohnWang <347220312@qq.com>
2023-03-15 00:47:36 +0800

    print.h support c++

--- gml/print.h
+++ gml/print.h
@@ -1,8 +1,10 @@
 #pragma once
 
+#ifndef __cplusplus
+
 static inline void _g_print_char(char x)
 {
-    printf("%c",x);
+    putchar(x);
 }
 
 static inline void _g_print_bool(bool x)
@@ -74,11 +76,73 @@ static inline void _g_print_cstring(const char *s)
                             char*:_g_print_cstring,\
                             const char*:_g_print_cstring)(x)
 
+#else
+
+static inline void print_object(char x)
+{
+    putchar(x);
+}
+
+static inline void print_object(bool x)
+{
+    if(x)
+        printf("true");
+    else
+        printf("false");
+}
+
+static inline void print_object(int x)
+{
+    printf("%d",x);
+}
+
+static inline void print_object(unsigned int x)
+{
+    printf("%u",x);
+}
+
+static inline void print_object(long x)
+{
+    printf("%ld",x);
+}
+
+static inline void print_object(unsigned long x)
+{
+    printf("%lu",x);
+}
+
+static inline void print_object(long long x)
+{
+    printf("%lld",x);
+}
+
+static inline void print_object(unsigned long long x)
+{
+    printf("%llu",x);
+}
+
+static inline void print_object(float x)
+{
+    printf("%g",x);
+}
+
+static inline void print_object(double x)
+{
+    printf("%g",x);
+}
+
+static inline void print_object(const char *s)
+{
+    printf("%s",s);
+}
+
+#endif
+
 #include"_print.inc"
 #define print(...) macro_cat(_g_print_,count_macro_args(__VA_ARGS__))(__VA_ARGS__)
 
 #define println(...) print(__VA_ARGS__,"\n")
 
 #include"_place_point.inc"
-#define _g_place_point_0() print(where_from(),"\n")
+#define _g_place_point_0() print(where_from(),(char){'\n'})
 #define place_point(...) macro_cat(_g_place_point_,count_macro_args(__VA_ARGS__))(__VA_ARGS__)
