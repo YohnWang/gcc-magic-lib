9c89fd08923751d7e6fdb63abfc974dca2f39df3
YohnWang <347220312@qq.com>
2023-04-01 04:23:27 +0800

    test_kit support for c++

--- gml/test_kit.h
+++ gml/test_kit.h
@@ -6,6 +6,15 @@
 #include<time.h>
 #include<stdint.h>
 
+static inline int64_t get_clock_monitonic_ns()
+{
+    struct timespec t = {};
+    clock_gettime(CLOCK_MONOTONIC, &t);
+    return t.tv_nsec+t.tv_sec*1000000000LL;
+}
+
+#ifndef __cplusplus
+#include"RAII.h"
 // performance statistics
 typedef struct time_spend_print_t
 {
@@ -13,13 +22,6 @@ typedef struct time_spend_print_t
     const char *fmt;
     bool once_flag;
 }time_spend_print_t;
-
-static inline int64_t get_clock_monitonic_ns()
-{
-    struct timespec t = {};
-    clock_gettime(CLOCK_MONOTONIC, &t);
-    return t.tv_nsec+t.tv_sec*1000000000LL;
-}
 
 attr_unused static inline void _g_time_spend_print(time_spend_print_t *time_spend)
 {
@@ -61,5 +63,68 @@ attr_unused static inline time_spend_get_t make_time_spend_get(int64_t *out)
 #define time_spend_get_guard RAII(time_spend_get_t,_g_time_spend_get)
 
 #define scope_time_get(tp) for(time_spend_get_guard _g_guard=make_time_spend_get(tp);!_g_guard.once_flag;_g_guard.once_flag=true)
+
+#else
+
+struct time_spend_print_t
+{
+    int64_t start=get_clock_monitonic_ns();
+    const char *fmt;
+    bool once_flag=true;
+
+    time_spend_print_t(const char *fmt=nullptr):fmt{fmt}{}
+
+    ~time_spend_print_t()
+    {
+        int64_t tns=get_clock_monitonic_ns();
+        const char *fmt=this->fmt?this->fmt:"";
+        fprintf(stdout,"\e[31m""%s: used %gms\n""\e[0m",fmt,(tns-start)/1000000.0);
+    }
+
+    time_spend_print_t& operator=(bool b)
+    {
+        once_flag=b;
+        return *this;
+    }
+
+    operator bool()
+    {
+        return once_flag;
+    }
+};
+
+#define scope_time_print(...) macro_cat(_g_scope_time_print_,count_macro_args(__VA_ARGS__))(__VA_ARGS__)
+#define _g_scope_time_print_0() _g_scope_time_print_1(where_from())
+#define _g_scope_time_print_1(name) for(time_spend_print_t _g_tguard(name);_g_tguard;_g_tguard=false)
+
+struct time_spend_get_t
+{
+    int64_t start=get_clock_monitonic_ns();
+    int64_t *out;
+    bool once_flag=true;
+
+    time_spend_get_t(int64_t *o):out{o}{}
+
+    ~time_spend_get_t()
+    {
+        *out=get_clock_monitonic_ns()-start;
+    }
+
+    time_spend_get_t& operator=(bool b)
+    {
+        once_flag=b;
+        return *this;
+    }
+
+    operator bool()
+    {
+        return once_flag;
+    }
+};
+
+#define scope_time_get(tp) for(time_spend_get_t _g_guard(tp);_g_guard;_g_guard=false)
+
+
+#endif
 
 #endif
